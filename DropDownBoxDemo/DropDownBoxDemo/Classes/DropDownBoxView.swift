ad548dbe52ab218e63c35c6d474e984abaa2cb7f
kang77649119 <77649119@qq.com>
2016-10-26 09:57:22 +0800

    优化：减少在引用界面出现过多代码

--- DropDownBoxDemo/DropDownBoxDemo/Classes/DropDownBoxView.swift
+++ DropDownBoxDemo/DropDownBoxDemo/Classes/DropDownBoxView.swift
@@ -11,9 +11,9 @@ import UIKit
 private let DropDownBoxCellId = "DropDownBoxCellId"
 private let DropDownBoxViewH:CGFloat = 200
 
-protocol DropDownBoxViewDelegate : class {
+@objc protocol DropDownBoxViewDelegate : class {
     
-    func dropDownBoxClicked(info:String,finished:()->())
+    @objc optional func dropDownBoxClicked(referenceView:UIView, info:String, finished:()->())
     
 }
 
@@ -24,6 +24,10 @@ class DropDownBoxView: UIView {
             self.tableView.reloadData()
         }
     }
+    
+    var referenceView : UIView?
+    
+    var delegate : DropDownBoxViewDelegate?
     
     lazy var tableView : UITableView =  {
         
@@ -37,8 +41,6 @@ class DropDownBoxView: UIView {
         return tableView
     
     }()
-    
-    var delegate : DropDownBoxViewDelegate?
 
     override init(frame: CGRect) {
         
@@ -47,6 +49,23 @@ class DropDownBoxView: UIView {
         
         // 初始化UI
         setupUI()
+    }
+    
+    init(referenceView : UIView) {
+        
+        self.referenceView = referenceView
+        
+        let viewF = referenceView.frame
+        let viewFrame = CGRect(x: viewF.origin.x, y: viewF.maxY, width: viewF.width, height: DropDownBoxViewH)
+        super.init(frame: viewFrame)
+        
+        if referenceView.classForCoder == UITextField.classForCoder() {
+            (referenceView as! UITextField).delegate = self
+        }
+        
+        // 初始化UI
+        setupUI()
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -86,9 +105,37 @@ extension DropDownBoxView : UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         let info = self.datas![indexPath.row]
-        delegate?.dropDownBoxClicked(info: info, finished: { 
+        
+        guard let refView = self.referenceView else {
+            return
+        }
+        
+        // 文本框
+        if refView.classForCoder == UITextField.classForCoder() {
+            (refView as! UITextField).text = info
             self.isHidden = true
-        })
+        } else {
+            
+            // 非文本框
+            delegate?.dropDownBoxClicked!(referenceView: refView, info: info, finished: { 
+                self.isHidden = true
+            })
+            
+        }
+        
+    }
+
+}
+
+extension DropDownBoxView : UITextFieldDelegate {
+
+    func textFieldShouldBeginEditing(_ textField: UITextField) -> Bool {
+        
+        self.isHidden = !self.isHidden
+        self.tableView.scrollToRow(at: IndexPath(row: 0, section: 0), at: UITableViewScrollPosition.top, animated: true)
+        
+        return false
+        
     }
 
 }
--- DropDownBoxDemo/DropDownBoxDemo/Classes/ViewController.swift
+++ DropDownBoxDemo/DropDownBoxDemo/Classes/ViewController.swift
@@ -12,7 +12,7 @@ private let screenW = UIScreen.main.bounds.width
 
 class ViewController: UIViewController {
     
-    // 文本框
+    // 控件1 文本框
     lazy var textFiled : UITextField = {
         
         let frame = CGRect(x: 5, y: 30, width: screenW - 10, height: 30)
@@ -29,17 +29,25 @@ class ViewController: UIViewController {
         
     }()
     
-    // 下拉框
-//    lazy var dropDownBoxView : DropDownBoxView = {
-//    
-//        let dropDownBoxView:DropDownBoxView = DropDownBoxView(frame: self.textFiled.frame)
-//        dropDownBoxView.delegate = self
-//        dropDownBoxView.isHidden = true
-//        return dropDownBoxView
-//    
-//    }()
+    // 控件2 Label
+    lazy var secTextFiled : UITextField = {
+        
+        let frame = CGRect(x: 5, y: 70, width: screenW - 10, height: 30)
+        let textField = UITextField(frame: frame)
+        textField.borderStyle = .line
+        let btn = UIButton(frame: CGRect(x: 0, y: 0, width: 30, height: 10))
+        btn.setTitle("+", for: .normal)
+        btn.setTitleColor(UIColor.black, for: .normal)
+        btn.addTarget(self, action: #selector(ViewController.showDropDownBox), for: .touchUpInside)
+        textField.rightView = btn
+        textField.rightViewMode = .always
+        
+        return textField
+        
+    }()
 
     var dropDownBoxView : DropDownBoxView?
+    var secDropDownBoxView : DropDownBoxView?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -55,33 +63,30 @@ extension ViewController {
     func setupUI() {
         
         self.view.addSubview(textFiled)
+        self.view.addSubview(secTextFiled)
         
-        // 创建下拉框控件
-        let dropDownBoxView = DropDownBoxView(frame: self.textFiled.frame)
-        dropDownBoxView.delegate = self
+        // 创建控件1的下拉框
+        let dropDownBoxView = DropDownBoxView(referenceView: textFiled)
         self.view.addSubview(dropDownBoxView)
         self.dropDownBoxView = dropDownBoxView
         
+        // 创建控件2的下拉框
+        let secDropDownBoxView = DropDownBoxView(referenceView: secTextFiled)
+        self.view.addSubview(secDropDownBoxView)
+        self.secDropDownBoxView = secDropDownBoxView
+        
         // 设置数据
         dropDownBoxView.datas = ["111","222","333","444","555","666"]
+        secDropDownBoxView.datas = ["AAA","BBB","CCC","DDD","EEE","FFF"]
     
     }
 
     // 显示下拉框
     @objc func showDropDownBox() {
-        UIView.animate(withDuration: 0.35) { 
-            self.dropDownBoxView?.isHidden = false
+        UIView.animate(withDuration: 0.35) {
+            self.dropDownBoxView?.isHidden = !self.dropDownBoxView!.isHidden
         }
     }
     
-}
-
-extension ViewController : DropDownBoxViewDelegate {
-
-    func dropDownBoxClicked(info : String, finished: () -> ()) {
-        self.textFiled.text = info
-        finished()
-    }
-
 }
 
